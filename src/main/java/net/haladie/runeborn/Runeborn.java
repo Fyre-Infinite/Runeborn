6fa5e6bef3d59d59b690783939b03bfbe976cc95
Fyre-Infinite <48196418+Fyre-Infinite@users.noreply.github.com>
2023-06-07 22:58:39 -0500

    Initial commit

---
+++ src/main/java/net/haladie/runeborn/Runeborn.java
@@ -0,0 +1,14 @@
+package net.haladie.runeborn;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Runeborn implements ModInitializer {
+	public static final String MOD_ID = "runeborn";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/haladie/runeborn/RunebornClient.java
@@ -0,0 +1,10 @@
+package net.haladie.runeborn;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RunebornClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
